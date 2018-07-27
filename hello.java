713f913c5bf62e04449a6975b428b5cf2221176c
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 12:40:11 +0530

    Update and rename leap.java to hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   
+ 
+        Scanner input= new Scanner(System.in);
+       long N= input.nextLong();
+       for(int i=0;i<N;i++) 
+           System.out.println("Hello");
+        
+      
+       
+  
+  }
+}
