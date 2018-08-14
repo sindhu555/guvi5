40f2f2701ff6811f48fb12862ef515109e75643c
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-14 10:28:59 +0530

    Update and rename minimum.c to sort.c

---
+++ sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int array[10];
+	int n,i,j,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for (i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(array[i]>array[j])
+			{
+				temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",array[i]);
+	}
+	
+
+
+	return 0;
+}
