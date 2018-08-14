8717d4b81ecaa6bf99cf5097b974fc589a9f6e8f
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-14 09:45:17 +0530

    Update arthi.c

--- arthi.c
+++ arthi.c
@@ -4,11 +4,8 @@ int main(void)
 {
 	int n,a,d,s;
 	int N,D,A,S;
-	printf("\n enter the number of terms:");
 	scanf("%d",&n);
-	printf("\n enter the start value:");
 	scanf("%d",&a);
-	printf("\n enter the difference:");
 	scanf("%d",&d);
 	N=n-1;
 	D=N*d;
