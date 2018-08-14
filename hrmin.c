7bbf05aa5385e5e4f134bae533887d1ad2bf4584
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-14 12:17:09 +0530

    Update hrmin.c

--- hrmin.c
+++ hrmin.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int hr,min,hr1,min1,hr2,min2;
 scanf("%d%d",&hr1,&min1);
@@ -7,4 +7,5 @@ scanf("%d%d",&hr2,&min2);
 hr=hr1-hr2;
 min=min1-min2;
 printf("%d\t%d",hr,min);
+  return 0;
 }
