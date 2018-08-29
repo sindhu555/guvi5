d0b5b1d171c82324527c98e76dffa8801b54e56d
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-30 00:27:14 +0530

    Update roman.java

--- roman.java
+++ roman.java
@@ -20,7 +20,7 @@ class Ideone
 				break;
 			case "IV": System.out.println("4");
 				break;
-			case "V: System.out.println("5");
+			case "V": System.out.println("5");
 				break;
 			case "VI": System.out.println("6");
 				break;
