27b9c6dc0f491bc4b996134bfa9e37bb14c05c44
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-30 00:13:06 +0530

    Create roman.java

---
+++ roman.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		switch(str)
+		{
+			case "1": System.out.println("1");
+				break;
+			case "2": System.out.println("2");
+				break;
+			case "3": System.out.println("3");
+				break;
+			case "4": System.out.println("4");
+				break;
+			case "5": System.out.println("5");
+				break;
+			case "6": System.out.println("6");
+				break;
+			case "7": System.out.println("7");
+				break;
+			case "8": System.out.println("8");
+				break;
+			case "9": System.out.println("9");
+				break;
+			case "10": System.out.println("1");
+				break;
+			case "11": System.out.println("11");
+				break;
+			case "12": System.out.println("12");
+				break;
+			case "13": System.out.println("13");
+				break;
+			case "14": System.out.println("14");
+				break;
+			case "15": System.out.println("15");
+				break;
+			case "16": System.out.println("16");
+				break;
+			case "17": System.out.println("17");
+				break;
+			case "18": System.out.println("18");
+				break;
+			case "19": System.out.println("19");
+				break;
+			case "20": System.out.println("20");
+				break;
+				
+			
+		}
+	}
+}
